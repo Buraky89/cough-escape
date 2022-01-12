e63277b5827171ddb09a3d7eff923b4a8a3256de
buraky89 <2487413+Buraky89@users.noreply.github.com>
2022-01-12 01:28:08 +0100

    include time-ticking and next locations

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <button @click="addRandomPlayer">Create</button>
+    <button @click="tickTime">Tick Time</button>
     
     <MainPlatform :players="players"/>
   </div>
@@ -14,6 +15,7 @@ export default {
   data: function() {
     return {
       max_id: 0,
+      time: 0,
       players: []
     };
   },
@@ -27,7 +29,27 @@ export default {
       var x = this.getRandomInt(500);
       var y = this.getRandomInt(500);
 
-      return {"id": id, "name": randomName, "x": x, "y": y};
+      var nextX = this.getRandomInt(500);
+      var nextY = this.getRandomInt(500);
+
+      return {"id": id, "name": randomName, "x": x, "y": y, "nextX": nextX, "nextY": nextY};
+    },
+    moveSinglePlayer(index){
+      var singlePlayer = this.players[index];
+      singlePlayer.x = singlePlayer.nextX;
+      singlePlayer.x = singlePlayer.nextY;
+      
+      var nextX = this.getRandomInt(500);
+      var nextY = this.getRandomInt(500);
+      
+      singlePlayer.nextX = nextX;
+      singlePlayer.nextY = nextY;
+    },
+    tickTime(){
+      this.time++;
+      for(var i = 0; i < this.players.length; i++){
+        this.moveSinglePlayer(i);
+      }
     },
     addRandomPlayer(){
       var randomPlayer = this.createRandomPlayer();
